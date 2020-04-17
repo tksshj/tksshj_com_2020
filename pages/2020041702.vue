c52a4ee1df7f7c443a555dd8aea54d2aea577874
tksshj <tksshj@gmail.com>
2020-04-17 12:28:52 +0900

    Add 2020041702

---
+++ pages/2020041702.vue
@@ -0,0 +1,133 @@
+<template>
+  <div>
+    <div class="tc-scroller" :style="{ height: height + 'px' }"></div>
+    <div id="tc-canvas-container" class="tc-canvas-container"></div>
+
+    <tc-content
+      date="2020-04-17_02, animation"
+      title="郵便局がなくなってる・・・！？"
+    >
+      <p>
+        上北沢というところに住んでいます。
+        ポストが目当てで郵便局へ行ったのですが、なくなっていました・・・。
+        他のところに移転したんだろうか、まあたぶんそうか。
+      </p>
+      <p>
+        しかし字が読みにくいですね。それと文章が長くなったらどうするべきか。
+      </p>
+    </tc-content>
+
+  </div>
+</template>
+
+<script>
+import * as P5 from 'p5'
+import TWEEN from '@tweenjs/tween.js'
+import TcContent from '../components/tc_content.vue'
+
+export default {
+  components: {
+    'tc-content': TcContent
+  },
+  head: {
+    title: '2020-04-17_02 - tksshj.com'
+  },
+  data() {
+    return {
+      nPages: 10,
+      height: 0,
+      p5App: null,
+      p5: null,
+      y: 0
+    }
+  },
+  methods: {
+    setupPage() {
+      this.height = window.innerHeight * this.nPages
+      this.p5App = new P5(this.sketch, 'tc-canvas-container')
+      this.setTween(0, false)
+    },
+    setTween(startY, inc) {
+      const coords = { y: 0 }
+      this.tween = new TWEEN.Tween(coords)
+                            .to({ y: this.height }, 10 * 1000)
+                            .easing(TWEEN.Easing.Sinusoidal.InOut)
+                            .onUpdate(() => {
+                              if (inc) {
+                                this.y = coords.y
+                              } else {
+                                this.y = this.height - coords.y
+                              }
+                            })
+                            .onComplete(() => {
+                              this.setTween(0, false)
+                            })
+                            .start()
+    },
+    position() {
+      let h = window.innerHeight
+      let y = this.y
+      return {
+        page: Math.floor(y / h),
+        progress: (y % h) / h
+      }
+    },
+    sketch(p5) {
+      this.p5 = p5
+      this.p5.setup = this.setup
+      this.p5.draw = this.draw
+      this.p5.windowResized = this.windowResized
+    },
+    setup() {
+      let width = window.innerWidth
+      let height = window.innerHeight
+      this.p5.createCanvas(width, height, this.p5.WEBGL)
+    },
+    draw() {
+      TWEEN.update()
+
+      let isPortrait = this.p5.windowWidth < this.p5.windowHeight
+      let nHorizontalTiles = isPortrait ? 3 : parseInt(this.p5.windowWidth / 256)
+      let tileWidth = this.p5.windowWidth / nHorizontalTiles
+      let nVerticalTiles = parseInt(this.p5.windowHeight / tileWidth) + 1
+
+      let position = this.position()
+
+      this.p5.stroke(216)
+
+      for (let row = 0; row < nVerticalTiles; row++) {
+        for (let col = 0; col < nHorizontalTiles; col++) {
+          this.p5.push()
+          this.p5.translate(tileWidth * (col - nHorizontalTiles * 0.5), tileWidth * row - this.p5.windowHeight * 0.5)
+          this.p5.rotate(Math.PI * position.progress)
+          this.p5.square(0, 0, tileWidth)
+          this.p5.pop()
+        }
+      }
+    },
+    windowResized() {
+      console.log('resized')
+      this.p5.resizeCanvas(this.p5.windowWidth, this.p5.windowHeight);
+    }
+  },
+  mounted() {
+    this.setupPage()
+  },
+  beforeDestroy() {
+    this.p5.remove()
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.tc-scroller {
+  width: 100vw;
+}
+.tc-canvas-container {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
