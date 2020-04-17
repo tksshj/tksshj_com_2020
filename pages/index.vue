12070f758497261d4e14a136c6e7cd76ecab8d1b
tksshj <tksshj@gmail.com>
2020-04-17 12:05:51 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,45 +3,34 @@
     <a href="/" class="tc-site-title">tksshj.com</a>
 
     <div class="tc-pages">
-
-      <a href="2020041604">
-        <h2>スクエアのスクエアの運転の</h2>
-        <p>2020-04-16_04</p>
-      </a>
-
-      <a href="2020041603">
-        <h2>スクエアのスクエアのスクエアの</h2>
-        <p>2020-04-16_03</p>
-      </a>
-
-      <a href="2020041602">
-        <h2>スクエアもするし自動運転もする</h2>
-        <p>2020-04-16_02</p>
-      </a>
-
-      <a href="2020041601">
-        <h2>スクロールするしスクエアもまわす</h2>
-        <p>2020-04-16_01</p>
-      </a>
-
-      <a href="2020041503">
-        <h2>自動再生もする</h2>
-        <p>2020-04-15_03</p>
-      </a>
-
-      <a href="2020041502">
-        <h2>スクロールするしサークルもする</h2>
-        <p>2020-04-15_02</p>
-      </a>
-
-      <a href="2020041501">
-        <h2>スクロールする</h2>
-        <p>2020-04-15_01</p>
-      </a>
-
+      <template v-for="page in pages">
+        <a :href="page.path">
+          <h2>{{ page.title }}</h2>
+          <p>{{ page.date }}</p>
+        </a>
+      </template>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      pages: [
+        { path: "2020041701", date: "2020-04-17_01", title: "index.vueの幅がおかしい？" },
+        { path: "2020041604", date: "2020-04-16_04", title: "スクエアのスクエアの運転の" },
+        { path: "2020041603", date: "2020-04-16_03", title: "スクエアのスクエアのスクエアの" },
+        { path: "2020041602", date: "2020-04-16_02", title: "スクエアもするし自動運転もする" },
+        { path: "2020041601", date: "2020-04-16_01", title: "スクロールするしスクエアもまわす" },
+        { path: "2020041503", date: "2020-04-15_03", title: "自動再生もする" },
+        { path: "2020041502", date: "2020-04-15_02", title: "スクロールするしサークルもする"},
+        { path: "2020041501", date: "2020-04-15_01", title: "スクロールする" }
+      ]
+    }
+  }
+}
+</script>
 
 <style scoped lang="scss">
 .tc-page-index {
@@ -51,7 +40,7 @@
   .tc-pages {
     a {
       display: block;
-      width: 100%;
+      width: calc(100% - 32px);
       padding: 24px 16px;
       &:hover {
         background-color: #FAFAFA;
