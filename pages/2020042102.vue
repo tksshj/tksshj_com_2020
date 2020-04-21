5701284963555c1347f754bcff9a3ab9a7d1b25f
tksshj <tksshj@gmail.com>
2020-04-22 08:05:33 +0900

    Update 2020042102

--- pages/2020042102.vue
+++ pages/2020042102.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
+    <div class="tc-scroller" :style="{ height: height + 'px' }"></div>
     <div id="tc-canvas-container" class="tc-canvas-container"></div>
 
     <tc-page-content
       title="ミニマルテクノ"
-      description="2020-04-21_02, animation"
+      description="2020-04-21_02, scroll"
     >
       <p>
-        最近Spotifyのミニマルテクノのプレイリストをかけて仕事しています。
-        自分のなかで年に数回ブームが来ます。アーティストが誰かとか、なにも詳しくなっていかないです。
+        最近Spotifyのミニマルテクノのプレイリストをかけて仕事してます。
+        自分のなかで年に数回ブームが来るんですが、アーティストとかは誰も知らないし、まったく詳しくなっていかないです。
+        プレイリストを公開していくれている人にほんと感謝。Spotifyにも感謝。
       </p>
       <p>
-        それで規則正しい、カクカクした動きにしようと思ったんですが、なんかいまいち好きな感じになりませんでした。。。
-        んー色かな？回転の軸の気もする。カクカク、リトライしよ。
+        アニメーション、最初につくったものがどうしても気に入らなかったので作り直しました。
+        下にスクロールしてください。
       </p>
       <p>
-        音やりたいけど今日はもう時間がなさそう。
+        今日は結局シンセができなかったな、、、時間欲しい。
       </p>
+
     </tc-page-content>
 
   </div>
@@ -24,8 +27,6 @@
 
 <script>
 import * as P5 from 'p5'
-import TWEEN from '@tweenjs/tween.js'
-import * as Tone from "tone";
 import TcPageContent from '../components/tc_page_content.vue'
 
 export default {
@@ -37,48 +38,21 @@ export default {
   },
   data() {
     return {
-      nPages: 3,
+      nPages: 10,
       height: 0,
       p5App: null,
       p5: null,
-      squares: [],
-      y: 0,
-      toneStated: false
+      squares: []
     }
   },
   methods: {
-    play() {
-      if (!this.toneStated) {
-        Tone.start()
-      }
-      let noise = new Tone.NoiseSynth().toMaster()
-      noise.triggerAttackRelease('8n')
-    },
     setupPage() {
       this.height = window.innerHeight * this.nPages
       this.p5App = new P5(this.sketch, 'tc-canvas-container')
-      this.setTween(0, false)
-    },
-    setTween(startY, inc) {
-      const coords = { y: 0 }
-      this.tween = new TWEEN.Tween(coords)
-                            .to({ y: this.height }, 10 * 1000)
-                            .easing(TWEEN.Easing.Linear.None) //Sinusoidal.InOut)
-                            .onUpdate(() => {
-                              if (inc) {
-                                this.y = coords.y
-                              } else {
-                                this.y = this.height - coords.y
-                              }
-                            })
-                            .onComplete(() => {
-                              this.setTween(coords.y, !inc)
-                            })
-                            .start()
     },
     position() {
       let h = window.innerHeight
-      let y = this.y
+      let y = window.scrollY
       return {
         page: Math.floor(y / h),
         progress: (y % h) / h
@@ -98,61 +72,42 @@ export default {
       /* this.p5.frameRate(10) */
 
       let isPortrait = this.p5.windowWidth < this.p5.windowHeight
-      let nHorizontalTiles = isPortrait ? 4 : parseInt(this.p5.windowWidth / 256)
+      let nHorizontalTiles = isPortrait ? 5 : parseInt(this.p5.windowWidth / 128)
       let tileWidth = this.p5.windowWidth / nHorizontalTiles
       let nVerticalTiles = parseInt(this.p5.windowHeight / tileWidth) + 2
       for (let row = 0; row < nVerticalTiles; row++) {
         for (let col = 0; col < nHorizontalTiles + 2; col++) {
           this.squares.push({
+            row: row,
+            col: col,
             x: tileWidth * (col - nHorizontalTiles * 0.5),
             y: tileWidth * row - this.p5.windowHeight * 0.5,
-            width: tileWidth,
-            direction: parseInt(this.p5.random(0, 3))
+            width: tileWidth
           })
         }
       }
     },
     draw() {
-      TWEEN.update()
-
-      /* let changeScaleRatio = (this.p5.frameCount % 20 == 0) */
-
       let position = this.position()
-      let pos = parseInt(position.progress * 15.0)
-      /* this.p5.clear() */
-      /* this.p5.background(216) */
+
+      /* this.p5.clear()
+       * this.p5.background(216) */
+
       this.p5.stroke(192)
       this.p5.strokeWeight(1)
 
       for (let i = 0; i < this.squares.length; i++) {
         let square = this.squares[i]
+
         this.p5.push()
         this.p5.translate(square.x, square.y)
 
-        /* this.p5.fill(216) */
+        let ws = (position.progress < 0.5 ? 1.0 - position.progress : position.progress)
 
-        let move = square.width / 15.0 * pos
-
-
-        this.p5.rotate(move)
-
-        switch (i) {// square.direction) {
-          case 0:
-            this.p5.translate(move, move)
-            break
-          case 1:
-            this.p5.translate(move, 0)
-            break
-          case 2:
-            this.p5.translate(0,  move)
-            break
-          case 3:
-            this.p5.translate(-move, 0)
-            break
-        }
-
-        this.p5.fill(255 - (i % 7) * 8)
-        this.p5.square(0, 0, square.width)
+        this.p5.translate(square.width * (ws - 1.0), square.width * (ws - 1.0))
+        /* console.log(position.progress)
+         * console.log(ws) */
+        this.p5.square(-square.width * 0.5, -square.width * 0.5, square.width * ws)
         this.p5.pop()
 
       }
