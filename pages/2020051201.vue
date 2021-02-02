87154900a6958aa610d0c8afb1e67077940a5ae6
tksshj <tksshj@gmail.com>
2021-02-03 00:26:08 +0900

    Update 2020051201

--- pages/2020051201.vue
+++ pages/2020051201.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
-    <div id="tc-canvas-container" class="tc-canvas-container"></div>
-
-    <tc-page-content :playButton="true">
+  <tc-page>
+    <template slot="animation">
+      <tc-animation />
+    </template>
+    <template slot="main">
       <p>
         今週やることが溢れてるので完全に現実逃避なんですが、ツイッタにシェアさせていただきます時に、
         ちゃんとしようと思って今日はなんだっけ、バランタインの安いやつ。やべえな明日から本気出す。
@@ -11,182 +12,18 @@
       <p>
         アニメーションはなんか虫っぽくしたかったんですけど、ちょっとちがいますね、でも好きです。
       </p>
-    </tc-page-content>
-
-  </div>
+    </template>
+  </tc-page>
 </template>
 
 <script>
-import * as P5 from 'p5'
-import TWEEN from '@tweenjs/tween.js'
-import TcPageContent from '../components/tc_page_content.vue'
+import TcPage from '../components/tc_page.vue'
+import TcAnimation from '../components/animations/2020051201.vue'
 
 export default {
   components: {
-    'tc-page-content': TcPageContent
-  },
-  data() {
-    return {
-      nPages: 10,
-      height: 0,
-      p5App: null,
-      p5: null,
-      tileWidth: 0,
-      nHorizontalTiles: 0,
-      nVerticalTiles: 0,
-      y: 0
-    }
-  },
-  methods: {
-    setupPage() {
-      this.height = window.innerHeight * this.nPages
-      this.p5App = new P5(this.sketch, 'tc-canvas-container')
-      this.setTween(0, true)
-    },
-    setTween(startY, inc) {
-      let coords = { y: 0 }
-      this.tween = new TWEEN.Tween(coords)
-                            .to({ y: this.height }, 20 * 1000)
-                            .easing(TWEEN.Easing.Linear.None)
-                            .onUpdate(() => {
-                              if (inc) {
-                                this.y = startY + coords.y
-                              } else {
-                                this.y = startY - coords.y
-                              }
-                            })
-                            .onComplete(() => {
-                              if (inc) {
-                                this.setTween(startY + coords.y, !inc)
-                              } else {
-                                this.setTween(startY - coords.y, !inc)
-                              }
-                            })
-                            .start()
-    },
-    position() {
-      let h = window.innerHeight
-      let y = this.y
-      return {
-        page: Math.floor(y / h),
-        progress: (y % h) / h
-      }
-    },
-    sketch(p5) {
-      this.p5 = p5
-      this.p5.setup = this.setup
-      this.p5.draw = this.draw
-      this.p5.windowResized = this.windowResized
-    },
-    setup() {
-      let width = window.innerWidth
-      let height = window.innerHeight
-      this.p5.createCanvas(width, height, this.p5.WEBGL)
-
-      let isPortrait = this.p5.windowWidth < this.p5.windowHeight
-      this.nHorizontalTiles = isPortrait ? 3 : parseInt(this.p5.windowWidth / 256)
-      this.tileWidth = this.p5.windowWidth / this.nHorizontalTiles
-      this.nHorizontalTiles = this.nHorizontalTiles + 4
-      this.nVerticalTiles = parseInt(this.p5.windowHeight / this.tileWidth) + 5
-
-      this.p5.frameRate(20)
-
-    },
-    translateTile(page, progress, row, col) {
-      this.p5.translate(this.tileWidth * (col - 2), this.tileWidth * (row - 2))
-      if (row % 2 == page % 2) {
-        if (col % 2 == page % 2) {
-          this.p5.translate(this.tileWidth * progress, 0)
-        } else {
-          this.p5.translate(0, this.tileWidth * progress)
-        }
-      } else {
-        if (col % 2 == page % 2) {
-          this.p5.translate(0, this.tileWidth * progress * -1.0)
-        } else {
-          this.p5.translate(this.tileWidth * progress * -1.0, 0)
-        }
-      }
-    },
-    fillColor(page, row, col) {
-      let pos = 0
-      if (row % 2 == 0 && col % 2 == 0) {
-        pos = 0
-      } else if (row % 2 == 0 && col % 2 == 1) {
-        pos = 3
-      } else if (row % 2 == 1 && col % 2 == 0) {
-        pos = 1
-      } else {
-        pos = 2
-      }
-      return 255 - ((pos + page) % 4) * 8
-    },
-    draw() {
-      TWEEN.update()
-      let position = this.position()
-      let page = position.page
-      let progress = position.progress
-
-      /* this.p5.clear() */
-      this.p5.stroke(216)
-
-      this.p5.rotate(Math.PI * (page + progress) * 0.01)
-      for (let row = 0; row < this.nVerticalTiles; row++) {
-        for (let col = 0; col < this.nHorizontalTiles; col++) {
-          this.p5.push()
-          this.p5.translate(this.p5.windowWidth * -0.5, this.p5.windowHeight * -0.5)
-          this.translateTile(page, progress, row, col)
-          this.p5.rotate(Math.PI * (page + progress) * 0.5)
-          this.p5.fill(this.fillColor(page, row, col))
-          this.p5.circle(this.tileWidth * this.p5.random(0.95, 1.05),
-                         this.tileWidth * this.p5.random(0.95, 1.05),
-                         this.tileWidth * (progress < 0.5 ? 1.0 - progress : progress))
-          this.p5.pop()
-        }
-      }
-    },
-    windowResized() {
-      this.p5.resizeCanvas(this.p5.windowWidth, this.p5.windowHeight);
-    }
-  },
-  mounted() {
-    this.setupPage()
-  },
-  beforeDestroy() {
-    this.p5.remove()
-    if (this.player) {
-      this.player.stop()
-    }
+    'tc-page': TcPage,
+    'tc-animation': TcAnimation
   }
 }
 </script>
-
-<style scoped lang="scss">
-.tc-scroller {
-  width: 100vw;
-}
-.tc-canvas-container {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-}
-.tc-play-button-row {
-  padding: 8px 0 32px;
-  display: flex;
-  justify-content: center;
-  .tc-play-button {
-    width: 97.5%;
-    padding: 4px 16px;
-    text-align: center;
-    border: solid 1px #5F5F7F;
-  }
-}
-a {
-  text-decoration: underline;
-}
-.tc-animation-description {
-  margin: 24px 0 0;
-}
-</style>
