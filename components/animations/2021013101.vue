7d8b60eab95d4d80564399a24bf1524edc22475d
tksshj <tksshj@gmail.com>
2021-02-02 13:03:00 +0900

    Update 2021013101

---
+++ components/animations/2021013101.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="tc-animation" class="tc-animation" />
+</template>
+
+<script>
+import * as P5 from 'p5'
+
+export default {
+  data() {
+    return {
+      p5App: null,
+      p5: null,
+      squares: []
+    }
+  },
+  methods: {
+    setupPage() {
+      this.p5App = new P5(this.sketch, 'tc-animation')
+    },
+    sketch(p5) {
+      this.p5 = p5
+      this.p5.setup = this.setup
+      this.p5.draw = this.draw
+      this.p5.windowResized = this.windowResized
+    },
+    setup() {
+      let width = window.innerWidth
+      let height = window.innerHeight
+      this.p5.createCanvas(width, height)
+      this.p5.frameRate(20)
+      this.reset()
+    },
+    reset() {
+      let w = Math.max(this.p5.width, this.p5.height) * 0.1
+      for (let y = 0; y < (this.p5.height / w + 1); y++) {
+        for (let x = 0; x < (this.p5.width / w + 1); x++) {
+          this.squares.push({
+            x: w * x,
+            y: w * y,
+            w: w,
+            borderWidth: this.p5.random(w),
+            color: 255 - this.p5.random(64)
+          })
+        }
+      }
+    },
+    draw() {
+      this.p5.clear()
+      this.p5.background(216)
+
+      for (let i = 0; i < this.squares.length; i++) {
+        let square = this.squares[i]
+        this.p5.strokeWeight(square.borderWidth)
+        this.p5.stroke(square.color)
+        this.p5.rect(square.x, square.y, square.w, square.w)
+
+        square.borderWidth -= 2
+        if (square.borderWidth <= 0) {
+          square.borderWidth = this.p5.random(square.w)
+        }
+        square.color -= 1
+        if (square.color <= 255 - 64) {
+          square.color = 255 - this.p5.random(64)
+        }
+      }
+    },
+    windowResized() {
+      this.p5.resizeCanvas(this.p5.windowWidth, this.p5.windowHeight)
+      this.reset()
+    }
+  },
+  mounted() {
+    this.setupPage()
+  },
+  beforeDestroy() {
+    this.p5.remove()
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.tc-animation {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- pages/2021013101.vue
+++ pages/2021013101.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-    <div id="tc-canvas-container" class="tc-canvas-container"></div>
-
-    <tc-page-content :playButton="true">
+  <tc-page>
+    <template slot="animation">
+      <tc-animation />
+    </template>
+    <template slot="main">
       <p>
-        Dewar'sが1300円くらい、BEEFEATERが1000円くらい、EARLY TIMESが1600円くらい。
+        Dewar'sが1300円くらい、BEEFEATERが1000円くらい、EARLY TIMESが1200円くらい。
       </p>
       <p>
         Dewar'sはソーダ割り、BEEFEATERもソーダ割り、EARLY TIMESはストレート。
@@ -22,98 +23,18 @@
       <p>
         アニメーション、適当だけどまあまあ好き。
       </p>
-    </tc-page-content>
-
-  </div>
+    </template>
+  </tc-page>
 </template>
 
 <script>
-import * as P5 from 'p5'
-import TcPageContent from '../components/tc_page_content.vue'
+import TcPage from '../components/tc_page.vue'
+import TcAnimation from '../components/animations/2021013101.vue'
 
 export default {
   components: {
-    'tc-page-content': TcPageContent
-  },
-  data() {
-    return {
-      p5App: null,
-      p5: null,
-      squares: []
-    }
-  },
-  methods: {
-    setupPage() {
-      this.p5App = new P5(this.sketch, 'tc-canvas-container')
-    },
-    sketch(p5) {
-      this.p5 = p5
-      this.p5.setup = this.setup
-      this.p5.draw = this.draw
-      this.p5.windowResized = this.windowResized
-    },
-    setup() {
-      let width = window.innerWidth
-      let height = window.innerHeight
-      this.p5.createCanvas(width, height)
-      this.p5.frameRate(20)
-
-      let w = Math.max(width, height) * 0.1
-      for (let y = 0; y < (height / w + 1); y++) {
-        for (let x = 0; x < (width / w + 1); x++) {
-          this.squares.push({
-            x: w * x,
-            y: w * y,
-            w: w,
-            borderWidth: this.p5.random(w),
-            color: 255 - this.p5.random(64)
-          })
-        }
-      }
-    },
-    draw() {
-      this.p5.clear()
-      this.p5.background(216)
-
-      for (let i = 0; i < this.squares.length; i++) {
-        let square = this.squares[i]
-        this.p5.strokeWeight(square.borderWidth)
-        this.p5.stroke(square.color)
-        this.p5.rect(square.x, square.y, square.w, square.w)
-
-        square.borderWidth -= 2
-        if (square.borderWidth <= 0) {
-          square.borderWidth = this.p5.random(square.w)
-        }
-        square.color -= 1
-        if (square.color <= 255 - 64) {
-          square.color = 255 - this.p5.random(64)
-        }
-      }
-    },
-    windowResized() {
-      console.log('resized')
-      this.p5.resizeCanvas(this.p5.windowWidth, this.p5.windowHeight);
-    }
-  },
-  mounted() {
-    this.setupPage()
-  },
-  beforeDestroy() {
-    this.p5.remove()
+    'tc-page': TcPage,
+    'tc-animation': TcAnimation
   }
 }
 </script>
-
-<style scoped lang="scss">
-.tc-scroller {
-  width: 100vw;
-}
-.tc-canvas-container {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-}
-</style>
